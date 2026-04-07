db00f7a278f0b283ef7923fea3d68d50234127e1
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-07 16:13:44 -0600

    build(community:pack): add package `astrocommunity.pack.json`
    https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/json

--- lua/community.lua
+++ lua/community.lua
@@ -90,6 +90,7 @@ return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
 
   -- ------------------------------------------------------------------------------------------- --
