ceb7d0d86c9ea8cc9b886bff11f2befcab2ea75e
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-07 16:14:56 -0600

    build(community:pack): add package `astrocommunity.pack.python`
    https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/python

--- lua/community.lua
+++ lua/community.lua
@@ -93,6 +93,7 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
 
   -- ------------------------------------------------------------------------------------------- --
   -- programming-language-support
