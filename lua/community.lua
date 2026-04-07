6e326cd70b662de6000cacea1e14570049930dd2
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-07 16:13:05 -0600

    build(community:pack): add package `astrocommunity.pack.html-css`
    https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/pack/html-css

--- lua/community.lua
+++ lua/community.lua
@@ -89,6 +89,7 @@ return {
   { import = "astrocommunity.pack.ansible" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.lua" },
 
   -- ------------------------------------------------------------------------------------------- --
