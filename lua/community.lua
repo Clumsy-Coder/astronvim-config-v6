fe6a363d5bd838a9a10345610471d6cb2d83616f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-07 18:55:29 -0600

    chore(community:recipes): add package `astrocommunity.recipes.heirline-nvchad-statusline`
    https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/recipes/heirline-nvchad-statusline

--- lua/community.lua
+++ lua/community.lua
@@ -115,6 +115,7 @@ return {
 
   -- ------------------------------------------------------------------------------------------- --
   -- recipes
+  -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" }, -- NVchad statusline theme
   { import = "astrocommunity.recipes.heirline-tabline-buffer-number" }, -- show buffer number before the filename. Run using :b<buffer number>
   { import = "astrocommunity.recipes.heirline-vscode-winbar" }, -- show breadcrumbs on the winbar (below bufferline)
 
