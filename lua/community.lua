b38ce84737831ef4bf6587725f69a2649b99aa83
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-07 18:42:47 -0600

    build(community:recipes): add package `astrocommunity.recipes.heirline-vscode-winbar`
    https://github.com/AstroNvim/astrocommunity/tree/main/lua/astrocommunity/recipes/heirline-vscode-winbar

--- lua/community.lua
+++ lua/community.lua
@@ -115,6 +115,7 @@ return {
 
   -- ------------------------------------------------------------------------------------------- --
   -- recipes
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" }, -- show breadcrumbs on the winbar (below bufferline)
 
   -- ------------------------------------------------------------------------------------------- --
   -- register
