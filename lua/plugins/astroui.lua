24d9322107290fab48ea516b7bbdbc44855040be
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-09 19:10:20 -0600

    build(plugins:astroui): add custom icons for plugin `folke/todo-comments.nvim`

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -23,6 +23,18 @@ return {
       },
     },
     -- Icons can be configured throughout the interface
+    -- obtained from:
+    --    https://github.com/datamonsterr/astronvim_config/blob/67bdd62/icons.lua
+    --    https://github.com/AstroNvim/AstroNvim/blob/0fee587489/lua/configs/cmp.lua
+    --    https://github.com/Clumsy-Coder/astronvim-config-v5/commit/c10e7df0f7e8c2a1686fd67815070f677be05105
+    --
+    -- https://github.com/microsoft/vscode/blob/main/src/vs/base/common/codicons.ts
+    -- https://github.com/microsoft/vscode/blob/84ba0e1/src/vs/base/common/codicons.ts
+    -- https://jdhao.github.io/2020/10/07/nvim_insert_unicode_char/#use-ctrl-v-to-insert-character
+    -- go to the above and then enter <c-v>u<unicode or hexcode> and the symbold should appear
+    -- or go here and upload the font file: https://mathew-kurian.github.io/CharacterMap/
+    -- find more here: https://www.nerdfonts.com/cheat-sheet
+    --   פּ ﯟ   蘒練 some other good icons
     icons = {
       -- configure the loading of the lsp in the status line
       LSPLoading1 = "⠋",
@@ -35,6 +47,16 @@ return {
       LSPLoading8 = "⠧",
       LSPLoading9 = "⠇",
       LSPLoading10 = "⠏",
+      -- used by plugin `folke/todo-comments.nvim`
+      TodoDone = " ",
+      TodoFix = " ",
+      TodoHack = " ",
+      TodoNote = " ",
+      TodoPerformance = " ",
+      -- TodoTest = "ﭧ ",
+      TodoTest = " ",
+      Todo = " ",
+      TodoWarn = " ",
     },
   },
 }
