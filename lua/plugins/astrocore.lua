c0f18840b7aa6875999159087b218b23e330f433
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2026-04-11 12:41:19 -0600

    build(plugins:astrocore): set whitespace characters
    obtained from
    
    https://github.com/Clumsy-Coder/astronvim-config-v5/blob/9572b5d/lua/plugins/astrocore.lua#L55-L58

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -51,6 +51,11 @@ return {
 
         -- color column
         colorcolumn = { 100 },
+
+        -- whitespace characters
+        list = true, -- show whitespace characters
+        listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣", eol = "↲" },
+        showbreak = "↪ ", -- string to put at the start of lines that have been wrapped
         -- spellfile
         spell = true, -- sets vim.opt.spell
         spelllang = "en_ca",
